2955bec9498a62b0212a73447aa41ad4b1183161
Romeo-25 <69173720+Romeo-25@users.noreply.github.com>
2022-10-12 18:10:49 -0600

    Delete GUIA 7 directory

--- GUIA 7/navegacion/src/screens/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-export default function About() {
-  return (
-    <View>
-      <Text> Estamos en About</Text>
-    </View>
-  );
-}
